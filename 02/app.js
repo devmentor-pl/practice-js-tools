48a152073462eeff7fc879812b4f523ee5b8fc4b
Szymon Rojek <sz.rojek@gmail.com>
2020-04-02 14:57:26 +0200

    added comment to the file app.js

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,14 @@
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
 console.log('Moja wersja Node.js to: v13.12.0');
+
+
+/*
+
+I want to display both consol.logs. In this case I have to type the command in the terminal: node ./02/app.js
+
+Then I will get a message:
+
+Działa! Gratuluję uruchomienia JS z poziomu Node.js!
+Moja wersja Node.js to: v13.12.0
+
+*/
