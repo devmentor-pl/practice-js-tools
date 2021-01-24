f0b7441bfc56447bb3ce7bf23fda196b746e8de4
Błażej <B-Wojtusciszyn@wp.pl>
2021-01-24 10:24:37 +0100

    Delete app.js

--- 02/app.js
+++
@@ -1,4 +0,0 @@
-console.log("Działa! Gratuluję uruchomienia JS z poziomu Node.js!");
-console.log("Moja wersja Node.js to: v15. 6.0");
-console.log("Działa! Gratuluję uruchomienia JS z poziomu Node.js!");
-console.log("Moja wersja Node.js to: v15. 6. 0");
