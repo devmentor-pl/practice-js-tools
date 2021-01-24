cec675fe2c152a52fc102a1c5f92e48118b66aa9
omenBlase <b-wojtusciszyn@wp.pl>
2021-01-24 10:22:40 +0100

    added version node

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,4 @@
-console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
-console.log('Moja wersja Node.js to: ');
+console.log("Działa! Gratuluję uruchomienia JS z poziomu Node.js!");
+console.log("Moja wersja Node.js to: v15. 6.0");
+console.log("Działa! Gratuluję uruchomienia JS z poziomu Node.js!");
+console.log("Moja wersja Node.js to: v15. 6. 0");
