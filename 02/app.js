69ed4dd1facf9e244dc69760b9819282a7366c2c
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2022-11-08 19:45:37 +0100

    Task 02

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,6 @@
+const NODE_V = '16.13.0';
+const NPM_V = '7.24.2';
+
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
-console.log('Moja wersja Node.js to: [tutaj wpisz swoją wersję]');
-console.log('Moja wersja npm to: [tutaj wpisz swoją wersję]');
+console.log(`Moja wersja Node.js to: ${NODE_V}`);
+console.log(`Moja wersja npm to: ${NPM_V}`);
