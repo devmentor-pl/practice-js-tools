52f23b4f0634469ed1895bb337da4bdc1d074746
mateuszjansowa <mateusz.jan.sowa@gmail.com>
2022-11-09 07:48:40 +0100

    update npm_v

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,5 @@
 const NODE_V = '14.20.0';
-const NPM_V = '7.24.2';
+const NPM_V = '8.19.1';
 
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
 console.log(`Moja wersja Node.js to: ${NODE_V}`);
