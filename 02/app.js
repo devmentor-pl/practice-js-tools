58acbd4a31e06a3604cb15a83b373698b35cc06a
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-11-29 11:42:17 +0100

    Add node and np versions

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,3 @@
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
 console.log('Moja wersja Node.js to: v16.15.1');
-console.log('Moja wersja npm to: 8.11.0');
+console.log('Moja wersja npm to: 8.11.0'); 
