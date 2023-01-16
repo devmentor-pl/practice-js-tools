4fb4ad381e7b52cc569880259ba58039ddf581a7
Ireneusz Cislo <irekc86@o2.pl>
2023-01-16 09:30:50 +0100

    change on version node and npm in app.js

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,3 @@
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
-console.log('Moja wersja Node.js to: [v16.13.1]');
-console.log('Moja wersja npm to: [8.1.2]');
+console.log('Moja wersja Node.js to: [v16.17.0]');
+console.log('Moja wersja npm to: [8.15.0]');
