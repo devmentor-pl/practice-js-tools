98aa0c9e2b925c09d188092681f6199b40cb5c30
Piotr <you@example.com>
2023-07-31 20:27:08 +0200

    add new file

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,3 @@
-console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
-console.log('Moja wersja Node.js to: v18.16.0');
-console.log('Moja wersja npm to: 9.5.1');
+console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!')
+console.log('Moja wersja Node.js to: v18.16.0')
+console.log('Moja wersja npm to: 9.5.1')
